a334d6fc1c2e10f0a07380c5588781431c98c6dd
Sean Rodriguez <sean.rodriguez@outlook.com>
2022-01-30 14:11:55 -0500

    Add shopping cart service

---
+++ src/app/kitchen-dashboard/services/shopping-cart.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { ShoppingList } from '../models/shopping-list.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ShoppingCartService {
+
+  private shoppingCartUrl = `${environment.recipeBookEndpoint}/shoppingcart`
+
+  constructor(private http: HttpClient) { }
+
+  getShoppingCart(): Observable<ShoppingList> {
+    return this.http.get<ShoppingList>(this.shoppingCartUrl);
+  }
+}
