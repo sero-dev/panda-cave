1519bd5a0e8ea893356bc98659281ac1a43326b6
Sean Rodriguez <sean.rodriguez@outlook.com>
2022-02-06 12:15:39 -0500

    Remove extra path variable

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  recipeBookEndpoint: 'http://api.serodev.com/api/recipe-book/recipe'
+  recipeBookEndpoint: 'http://api.serodev.com/api/recipe-book'
 };
