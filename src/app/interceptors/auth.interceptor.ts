7b76850fb23a716789ef7844cc3db2198292d772
Sean Rodriguez <sean.rodriguez@outlook.com>
2022-04-26 20:40:00 -0400

    Add interceptor adding the authorization header

---
+++ src/app/interceptors/auth.interceptor.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { AuthService } from '../services/auth.service';
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+
+  constructor(private authService: AuthService) {}
+
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    request = request.clone({
+      setHeaders: {
+        'Content-Type': 'application/json; charset=utf-8',
+        'Accept': 'application/json',
+        'Authorization': `Bearer ${this.authService.getToken()}`,
+      }
+    })
+    return next.handle(request);
+  }
+}
