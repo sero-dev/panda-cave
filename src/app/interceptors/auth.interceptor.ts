5e484f79b9b353e3f0408187cfb5fdffd48e692c
Sean Rodriguez <sean.rodriguez@outlook.com>
2022-04-30 19:02:14 -0400

    Add withCrediential option

--- src/app/interceptors/auth.interceptor.ts
+++ src/app/interceptors/auth.interceptor.ts
@@ -23,6 +23,7 @@ export class AuthInterceptor implements HttpInterceptor {
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     request = request.clone({
+      withCredentials: true,
       setHeaders: {
         'Content-Type': 'application/json; charset=utf-8',
         'Accept': 'application/json',
