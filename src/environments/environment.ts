e3e517fe0ecfea6c5284e3c21390d748e57db829
Sean Rodriguez <sean.rodriguez@outlook.com>
2022-05-04 08:29:02 -0400

    Add proxy config to bypass CORS issue

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  recipeBookEndpoint: 'http://localhost:8080/recipe-book',
-  authEndpoint: 'http://localhost:8080/auth'
+  recipeBookEndpoint: 'api/recipe-book',
+  authEndpoint: 'api/auth'
 };
